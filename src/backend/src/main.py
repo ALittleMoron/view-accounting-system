cbfaa05ceacf29af40793ab8f20ff9ecfa58a829
Lunev Dmitriy <dima.lunev14@gmail.com>
2022-07-17 17:34:08 +0300

    Удалил папку .vscode

--- src/backend/src/main.py
+++ src/backend/src/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
 
-from core.config import settings
-
 
 app = FastAPI()
--- src/backend/src/manage.py
+++ src/backend/src/manage.py
@@ -4,7 +4,7 @@ Django-like manage.py script for some administration utils.
 import sys
 
 
-from core.config import settings
+from core import settings
 from src.management import application
 from src.utils.exceptions import ExecuteError
 
